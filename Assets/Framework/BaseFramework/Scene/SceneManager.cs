d9f70b553ba1546592baa53cbae76a4b3186300c
namhyukseo <namhyuk2@nate.com>
2024-12-05 00:51:02 +0900

    작업
    작업

--- Assets/Framework/BaseFramework/Scene/SceneManager.cs
+++ Assets/Framework/BaseFramework/Scene/SceneManager.cs
@@ -1,10 +1,13 @@
-﻿using Framework.Singleton;
+﻿using Framework.Architecture;
+using Framework.Singleton;
+using UnityEngine.Assertions;
 
 namespace Framework.Scene
 {
-    public class SceneManager : Singleton<SceneManager>
+    public class SceneManager : Singleton<SceneManager>, IPostLateUpdate
     {
-        SceneModelBase currentScene = null;
+        WeakReference<SceneModelBase> currentScene  = new WeakReference<SceneModelBase>(null);
+        WeakReference<SceneModelBase> nextScene     = new WeakReference<SceneModelBase>(null);
 
         protected override void OnInit()
         {
@@ -21,17 +24,33 @@ namespace Framework.Scene
             SceneModelBase.CreateScene<T>(this.OnLoadSceneComplete);
         }
 
-        void OnLoadSceneComplete(SceneModelBase _scene)
+        private void OnLoadSceneComplete(SceneModelBase _scene)
         {
-            if(currentScene != null)
+            UnityEngine.Debug.Assert(!this.nextScene.IsAlive);
+            UnityEngine.Debug.Assert(_scene != null);
+
+            this.nextScene.Target = _scene;
+        }
+
+        public void OnPostLateUpdate(float _deltaTime)
+        {
+            if(this.nextScene.IsAlive)
             {
-                currentScene.OnExitScene(_scene);
+                if (this.currentScene.IsAlive)
+                {
+                    this.currentScene.Target.OnExitScene(this.nextScene.Target);
+                }
+                if (this.nextScene.IsAlive)
+                {
+                    this.nextScene.Target.OnEnterScene(this.currentScene.Target);
+                }
+
+                if(this.currentScene.IsAlive)
+                    this.currentScene.Target.Dispose();
+
+                this.currentScene.Target = this.nextScene.Target;
+                this.nextScene.Target = null;
             }
-            if(_scene != null)
-            {
-                _scene.OnEnterScene(currentScene);
-            }
-            currentScene = _scene;
         }
     }
 }
